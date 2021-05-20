150bb0c55d3fcf4cb90fddac8075ac81aa56d072
srikanthpragada <srikanthpragada@gmail.com>
2021-05-20 16:43:05 +0530

    20-MAY

---
+++ details.c
@@ -0,0 +1,10 @@
+// Program to display details
+// Date : 19-may-2021
+
+#include <stdio.h>
+
+void main()
+{
+   printf("Srikanth Pragada");
+   printf("srikanth@srikanthtechnologies.com");
+}
---
+++ welcome.c
@@ -0,0 +1,10 @@
+// My First C Program
+// Program to display welcome message
+// Date : 19-may-2021
+
+#include <stdio.h>
+
+void main()
+{
+    printf("Welcome To C");
+}
